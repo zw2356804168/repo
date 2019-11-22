67ba36e8b948e1cdaf9aa0a101eb2970fbaa45d8
xx <2356804168@qq.com>
2019-11-22 15:30:04 +0800

    删除springboot

--- batis2/src/main/java/com/hwua/mapper/DriverLicenseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hwua.mapper;
-
-import com.hwua.pojo.Driverlicense;
-
-public interface DriverLicenseMapper {
-    public Driverlicense findDriverLicenseById(Long did) throws Exception;
-}
